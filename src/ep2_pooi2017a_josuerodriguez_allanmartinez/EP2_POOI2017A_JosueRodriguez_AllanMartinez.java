06a6ebe72164482b52213b80d91f3aad82729587
a5 <a5@LS3-PC05>
2017-01-23 12:32:50 -0600

    Hola

---
+++ src/ep2_pooi2017a_josuerodriguez_allanmartinez/EP2_POOI2017A_JosueRodriguez_AllanMartinez.java
@@ -0,0 +1,25 @@
+/*
+    Titulo: EP2 Manejo de excepciones.
+    Autores:
+        Martinez Ramirez Allan Axel     MRAO150815
+        Rodriguez Perez Josue Eduardo   RPJO151978
+
+    Profesora: Alma Delia Nieto Diaz
+    Fecha: 
+*/
+package ep2_pooi2017a_josuerodriguez_allanmartinez;
+
+/**
+ *
+ * @author a5
+ */
+public class EP2_POOI2017A_JosueRodriguez_AllanMartinez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
